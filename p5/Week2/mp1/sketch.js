72db4706c006aa126f887e2703818b463db322ca
Rose Marshack <rose@posterchildren.com>
2020-01-28 10:41:59 -0600

    1

--- p5/Week2/mp1/sketch.js
+++ p5/Week2/mp1/sketch.js
@@ -1,30 +1,32 @@
 function setup() {
   // put setup code here
-  createCanvas(1000, 800);
-  rectMode(CENTER);
-  ellipseMode(CENTER);
+  createCanvas(1000, 800) ;
+  rectMode(CENTER) ;
+  ellipseMode(CENTER) ;
 }
 
 function draw() {
   // put drawing code here
-  background(100);
+  background(100) ;
   // stuff that we always want to show
 
 
   if (mouseIsPressed) {
     // stuff that shows when the mouse is pressed
-    ellipse(50, 50, 50, 50);
+      ellipse(50, 50, 50, 50);
 
+    } else {
+      // when the mouse isn't pressed!
+      rect(50, 50, 50, 50);
+    }
 
-  }
-
-  fill(0);
-  textSize(40);
-  text(mouseX.toFixed(0) + ", " + mouseY, 40, 40);
+    fill(0) ;
+    textSize(40) ;
+      text(mouseX.toFixed(0) + ", " + mouseY, 40, 40) ;
 }
 
 
 
 function mouseReleased() {
-  console.log(mouseX + ", " + mouseY);
+  console.log(mouseX + ", " + mouseY) ;
 }
