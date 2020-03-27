22f15f207790945fb774beaa202d1dad46c07597
Rose Marshack <rose@posterchildren.com>
2020-03-27 08:57:26 -0500

    teon debug 1

---
+++ p5/debug/teonMP2/sketch.js
@@ -0,0 +1,15 @@
+var c ;
+
+function setup() {
+  // put setup code here
+  createCanvas(500, 500);
+  c = loadFont("assets/royal.ttf") ;
+  textFont(c) ;
+}
+
+function draw() {
+  // put drawing code here
+
+text("hello!", 100, 100) ;
+
+}
