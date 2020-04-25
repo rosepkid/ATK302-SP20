9bbba0ce8929cbc47229aa98a4a8e260c895c4c4
Rose Marshack <rose@posterchildren.com>
2020-04-25 17:32:16 -0500

    added rose's room

--- p5/MP4/geoWelcomeWeek/sketch.js
+++ p5/MP4/geoWelcomeWeek/sketch.js
@@ -20,6 +20,8 @@ function setup() {
   createCanvas(displayWidth, displayHeight);
   num = 0;
   intervalCurrentPosition(positionPing, 5000);
+  places.push(new Place(40.47859881213726, -88.96815846900026, "Rose's Room", .02, reggieImg)); // new Place object, for CVA room 17
+
   places.push(new Place(40.50622797365503, -88.99051350503431, "CVA 17", .02, reggieImg)); // new Place object, for CVA room 17
   places.push(new Place(40.50715473783438, -88.99173550368103, "COB", .02, reggieImg)); // new Place object, for COB.... JUST SWITCHED TO NEW COORDINATES
   places.push(new Place(40.510824736433904, -88.99134151266699, "ISU College Bridge", .02, reggieImg)); // new Place object, for ISU bridge over College Ave
